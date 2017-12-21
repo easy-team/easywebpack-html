afb859be7bf8736cb851f9b1a90630077be91577
caoli <cl98914@alibaba-inc.com>
2017-12-21 14:04:38 +0800

    publish:3.0.0

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const EasyWebpack = require('easywebpack');
 exports.EasyWebpack = EasyWebpack;
 Object.assign(exports, EasyWebpack);
 exports.WebpackClientBuilder = require('./lib/client');
-exports.getWebpackConfig = config => {
-  return EasyWebpack.WebpackBuilder.getWebpackConfig(config, [exports.WebpackClientBuilder]);
+exports.getWebpackConfig = (config, option) => {
+  return EasyWebpack.WebpackBuilder.getWebpackConfig(config, [exports.WebpackClientBuilder], option);
 };
